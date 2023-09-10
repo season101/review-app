97a3fd05d411bb779b616cd3f676f28c34d3de00
Sijan Malla <sijan.malla101@gmail.com>
2023-09-09 23:01:55 -0500

    Added Random generation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 import reviews from './data';
 const App = () => {
   const [index, setIndex] = useState(0);
-  const { id, image, job, name, text } = reviews[index];
+  const { image, job, name, text } = reviews[index];
+
+  // Helper function to generate random number [0, reviews.length].
+  const returnRandom = () => {
+    const randomNumber = Math.ceil(Math.random() * reviews.length);
+    return randomNumber === index ? returnRandom() : randomNumber;
+  };
 
   return (
     <main>
@@ -17,27 +23,32 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
-        <p className="info">
-          <div className="btn-container">
-            <button
-              className="prev-btn"
-              onClick={() =>
-                index === 0 ? setIndex(reviews.length - 1) : setIndex(index - 1)
-              }
-            >
-              <FaChevronLeft />
-            </button>
-            <button
-              className="next-btn"
-              onClick={() =>
-                index === reviews.length - 1 ? setIndex(0) : setIndex(index + 1)
-              }
-            >
-              <FaChevronRight />
-            </button>
-          </div>
-          <button className="btn btn-hipster">surprise me</button>
-        </p>
+        <div className="btn-container">
+          <button
+            className="prev-btn"
+            onClick={() =>
+              index === 0 ? setIndex(reviews.length - 1) : setIndex(index - 1)
+            }
+          >
+            <FaChevronLeft />
+          </button>
+          <button
+            className="next-btn"
+            onClick={() =>
+              index === reviews.length - 1 ? setIndex(0) : setIndex(index + 1)
+            }
+          >
+            <FaChevronRight />
+          </button>
+        </div>
+        <button
+          className="btn btn-hipster"
+          onClick={() => {
+            setIndex(returnRandom());
+          }}
+        >
+          surprise me
+        </button>
       </article>
     </main>
   );
